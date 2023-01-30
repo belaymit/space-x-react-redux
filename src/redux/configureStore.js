167ddfa2ffa4393ef89c1f7e148c0664840d9378
belaymit <2belamit@gmail.com>
2023-01-30 16:45:05 +0300

    added profile reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,13 +2,17 @@ import { applyMiddleware, configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import missionReducer from './mission/mission';
+import reservedRocketsReducer, { getAllReservedRockets } from './profile/Profile';
 import rocketsReducer, { Rockets } from './rockets/rocket';
 
 const store = configureStore({
   reducer: {
     missionReducer,
     rocketsReducer,
+    reservedRocketsReducer,
   },
 }, applyMiddleware(thunk, logger));
 store.dispatch(Rockets());
+store.dispatch(getAllReservedRockets());
+
 export default store;
