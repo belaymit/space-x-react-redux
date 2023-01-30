5f57880a8fb107da5431a655960be1cd29e54e9a
belaymit <2belamit@gmail.com>
2023-01-30 11:23:15 +0300

    integrated live data

--- src/components/ui/SingleRocket.jsx
+++ src/components/ui/SingleRocket.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
-import { rocket } from '../../assets/files';
+import PropTypes from 'prop-types';
 
-const SingleRocket = () => (
-  <li className="rocket">
-    <img src={rocket} alt="" />
+const SingleRocket = ({ rocket }) => (
+  <li key={rocket.id} className="rocket">
+    <img src={rocket.image} alt={rocket.name} />
     <div className="single-rocket-container">
       <span className="rocket-name">
-        Rocket
+        {rocket.name}
       </span>
       <div className="rocket-detail">
-        <span className="reserved">Reserved</span>
-        &nbsp;
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Dolor voluptatibus odio deleniti nam, temporibus reiciendis
-        sed cumque repellat reprehenderit tenetur.
+        {rocket.reserved ? (
+          <span className={rocket.reserved ? 'reserved' : ''}>
+            Reserved
+          </span>
+        ) : (
+          ''
+        )}
+          &nbsp;
+        {rocket.description}
       </div>
       <button type="button" className="reserve-rocket">Reserve Rocket</button>
     </div>
   </li>
 );
 
+SingleRocket.propTypes = {
+  rocket: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    reserved: PropTypes.bool.isRequired,
+  }).isRequired,
+};
 export default SingleRocket;
